6006b68cda8c563fd7e1a40e3217f8d0bcc4b0d4
amandoee <amandoee@gmail.com>
2022-11-15 17:05:28 +0100

    ingen_cache

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/andreasmandoe/OneDrive - Aarhus universitet/OneDrive Uni/ComputerTeknologi/1. Semester/Programmering for computerteknologi/week12/git/assignment-12-amandoee/tests
-# Build directory: /Users/andreasmandoe/OneDrive - Aarhus universitet/OneDrive Uni/ComputerTeknologi/1. Semester/Programmering for computerteknologi/week12/git/assignment-12-amandoee/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(usage_test "tests")
-set_tests_properties(usage_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/andreasmandoe/OneDrive - Aarhus universitet/OneDrive Uni/ComputerTeknologi/1. Semester/Programmering for computerteknologi/week12/git/assignment-12-amandoee/tests/CMakeLists.txt;6;add_test;/Users/andreasmandoe/OneDrive - Aarhus universitet/OneDrive Uni/ComputerTeknologi/1. Semester/Programmering for computerteknologi/week12/git/assignment-12-amandoee/tests/CMakeLists.txt;0;")
